c68cb370fddf160646aee212bf14a1b7337ff416
Dennis Kemboi <denkogei11@gmail.com>
2025-08-06 21:39:07 +0300

    Ignore .vs folder and commit working files

---
+++ AfriERP/Pages/Employees.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AfriERP.Pages
+{
+    public class EmployeesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
